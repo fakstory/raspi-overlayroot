4d6648b34a37e90fd65a81d5f77cd3973d3e134a
Alex G <fakstory@gmail.com>
2019-06-27 15:46:04 -0400

    correcting a typo I see each time I boot...

--- overlayroot-motd.sh
+++ overlayroot-motd.sh
@@ -2,7 +2,7 @@ if grep -qs 'overlayroot' /proc/cmdline; then
 cat << EOF
 
 ==> WARNING: Overlayroot in effect.
-None of the changes you make will be preseved after reboot.
+None of the changes you make will be preserved after reboot.
 
 To disable overlayroot remove the 'overlayroot' argument
 from the kernel commandline.
